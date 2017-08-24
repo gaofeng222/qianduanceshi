b1eeb9ec26f4006ac05c8cd95b3bd08af109c9da
gaofeng222 <596642721@qq.com>
2017-08-24 08:52:28 +0800

    This a new testing on 8.24

--- demo10.js
+++ demo10.js
@@ -7,6 +7,7 @@ function Node(data,left,right){
     this.data = data;
     this.left = left;
     this.right = right;
+    this.show = show;
 }
 
 
@@ -30,18 +31,42 @@ function insert(data){
         while(true){
             parent = current;
             if(data < current.data){
-
+                current = current.left;
+                if(current == null){
+                    parent.left = n;
+                    break;
+                }
+            }else{
+                current = current.right;
+                if(current == null){
+                    parent.right = n;
+                    break;
+                }
             }
         }
     }
 }
 
 
+//中序遍历
+function inOrder(node){
+    if(!(node == null)){
+        inOrder(node.left);
+        console.log(node.show() + " ");
+        inOrder(node.right)
+    }
+}
 
-
-
-
-
+var nums = new BST();
+nums.insert(23)
+nums.insert(45)
+nums.insert(16)
+nums.insert(37)
+nums.insert(3)
+nums.insert(99)
+nums.insert(22)
+console.log("Ionder traversal: ")
+inOrder(nums.root)
 
 
 
